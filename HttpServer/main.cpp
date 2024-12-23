763d3277d476e749f66573bf29f32d965bf913f4
xll98 <49185575+xll98@users.noreply.github.com>
2024-12-23 11:35:07 +0800

    Update main.cpp

--- HttpServer/main.cpp
+++ HttpServer/main.cpp
@@ -1,11 +1,4 @@
-/*
- * @Author: mircle mircle.xue@mvtlabs.com
- * @Date: 2024-12-20 11:02:11
- * @LastEditors: mircle mircle.xue@mvtlabs.com
- * @LastEditTime: 2024-12-23 11:10:57
- * @FilePath: /MongooseHttp/HttpServer/main.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 #include "http_server.h"
 #define WEB_PORT "8080"
 
